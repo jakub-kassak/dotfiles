f865d080f32d7e3b24c0c1ca2703ada5efb4f6b1
Jakub Kaššák <jakubkassak@gmail.com>
2024-09-05 12:25:11 +0200

    wezterm v4

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,19 +6,41 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 
 local function depending_on_appearance(arg)
-	local appearance = wezterm.gui.get_appearance()
-	if appearance:find("Dark") then
-		return arg.dark
-	else
+	-- local appearance = wezterm.gui.get_appearance()
+	-- if appearance:find("Dark") then
+	-- 	return arg.dark
+	-- else
+	-- 	return arg.light
+	-- end
+	local hour = tonumber(os.date("%H"))
+
+	if hour >= 5 and hour < 20 then
 		return arg.light
+	else
+		return arg.dark
 	end
 end
+
+config.color_schemes = {
+	["AMOLED Dark"] = {
+		foreground = "#D0D0D0", -- Light gray text
+		background = "#000000", -- True black background
+		cursor_bg = "#D0D0D0",
+		cursor_border = "#D0D0D0",
+		cursor_fg = "#000000",
+		selection_bg = "#44475a",
+		selection_fg = "#FFFFFF",
+
+		ansi = { "#000000", "#FF5555", "#50FA7B", "#F1FA8C", "#BD93F9", "#FF79C6", "#8BE9FD", "#BFBFBF" },
+		brights = { "#4D4D4D", "#FF6E67", "#5AF78E", "#F4F99D", "#CAA9FA", "#FF92D0", "#9AEDFE", "#E6E6E6" },
+	},
+}
 
 config.initial_rows = 35
 config.initial_cols = 110
 config.color_scheme = depending_on_appearance({
-	light = "Tokyo Night Moon", -- "Vs Code Light+ (Gogh)", -- 3024 (light) (terminal.sexy)", --One Light (Gogh)", --Vs Code Light+ (Gogh)", -- Catppuccin Latte',
-	dark = "Tokyo Night", -- "Vs Code Dark+ (Gogh)", --3024 (dark) (terminal.sexy)", -- Catppuccin Mocha",
+	light = "PaperColor Light (base16)", --"Gruvbox light, hard (base16)", -- "Tokyo Night Moon", -- "Vs Code Light+ (Gogh)", -- 3024 (light) (terminal.sexy)", --One Light (Gogh)", --Vs Code Light+ (Gogh)", -- Catppuccin Latte',
+	dark = "Gruvbox dark, hard (base16)", -- "Papercolor Dark (Gogh)", --"Tokyo Night", -- "Vs Code Dark+ (Gogh)", --3024 (dark) (terminal.sexy)", -- Catppuccin Mocha",
 })
 config.use_fancy_tab_bar = false
 config.tab_max_width = 32
@@ -36,6 +58,14 @@ config.font_size = 15
 
 local act = wezterm.action
 config.keys = {
+	{
+		key = "Enter",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SpawnCommandInNewWindow({
+			args = { "bash" },
+		}),
+	},
+
 	-- Copy to clipboard using Ctrl+C
 	{
 		key = "c",
@@ -53,20 +83,21 @@ config.keys = {
 			end
 		end),
 	},
-	-- { key = "c", mods = "CTRL", action = act({ CopyTo = "Clipboard" }) },
-	-- Paste from clipboard using Ctrl+V
+	{ key = "V", mods = "CTRL|SHIFT", action = act.SendKey({ key = "v", mods = "CTRL" }) },
 	{ key = "v", mods = "CTRL", action = act({ PasteFrom = "Clipboard" }) },
-	{ key = "v", mods = "CTRL|SHIFT", action = act.SendKey({ key = "v", mods = "CTRL" }) },
-	-- Close the current tab
+
+	-- { key = "c", mods = "CTRL", action = act.CopyTo("Clipboard") },
+	-- { key = "c", mods = "SUPER", action = act.SendKey({ key = "c", mods = "CTRL" }) },
+	-- -- Close the current tab
 	{ key = "w", mods = "CTRL", action = act({ CloseCurrentTab = { confirm = true } }) },
 
 	-- Search with Crtl+F
-	{ key = "f", mods = "CTRL|SHIFT", action = act.Search("CurrentSelectionOrEmptyString") },
+	-- { key = "f", mods = "CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
 
 	-- Show tab navigator
-	-- { key = "p", mods = "CTRL", action = act.ShowTabNavigator },
+	{ key = "p", mods = "SUPER", action = act.ShowTabNavigator },
 	-- Show launcher menu
-	{ key = "P", mods = "CTRL|SHIFT", action = act.ShowLauncher },
+	{ key = "P", mods = "SUPER|SHIFT", action = act.ShowLauncher },
 	-- Spawn a new tab
 	{ key = "t", mods = "CTRL", action = act({ SpawnTab = "CurrentPaneDomain" }) },
 
@@ -74,7 +105,8 @@ config.keys = {
 	{ key = "|", mods = "CTRL|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	-- Underscore (_) -> vertical split
 	{ key = "_", mods = "CTRL|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	-- Use CTRL + [h|j|k|l] to move between panes
+
+	-- Use CTRL + SHIFT + [h|j|k|l] to move between panes
 	{ key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
 	{ key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
 	{ key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
@@ -90,7 +122,7 @@ config.keys = {
 
 	-- Rename current tab with CTRL + SHIFT + e
 	{
-		key = "e",
+		key = "E",
 		mods = "CTRL|SHIFT",
 		action = act.PromptInputLine({
 			description = "Enter new name for tab",
@@ -103,7 +135,16 @@ config.keys = {
 	},
 
 	-- Move to a pane (prompt to which one)
-	{ mods = "CTRL|SHIFT", key = "m", action = act.PaneSelect },
+	{ mods = "CTRL|SHIFT", key = "M", action = act.PaneSelect },
+
+	-- Switch to tab 1, 2, ...  <C-1,2,...>
+	{ key = "1", mods = "CTRL", action = wezterm.action({ ActivateTab = 0 }) },
+	{ key = "2", mods = "CTRL", action = wezterm.action({ ActivateTab = 1 }) },
+	{ key = "3", mods = "CTRL", action = wezterm.action({ ActivateTab = 2 }) },
+	{ key = "4", mods = "CTRL", action = wezterm.action({ ActivateTab = 3 }) },
+	{ key = "5", mods = "CTRL", action = wezterm.action({ ActivateTab = 4 }) },
+	{ key = "6", mods = "CTRL", action = wezterm.action({ ActivateTab = 5 }) },
+	{ key = "7", mods = "CTRL", action = wezterm.action({ ActivateTab = 6 }) },
 }
 
 config.pane_focus_follows_mouse = false
